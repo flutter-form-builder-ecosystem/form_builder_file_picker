f76598a5b622d03851a5e94bc6d17819b56f8d71
Matias de Andrea <deandreamatias@gmail.com>
2024-08-11 18:26:27 +0200

    feat: create new web an android example setup

---
+++ example/android/app/src/main/kotlin/com/example/form_builder_file_picker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.form_builder_file_picker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- example/android/app/src/main/kotlin/dev/formbuilderfilepicker/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.formbuilderfilepicker.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
