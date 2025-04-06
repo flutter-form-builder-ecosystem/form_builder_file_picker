c096855705d990ec1e0e4de0327f0eed6b22c554
Matias de Andrea <deandreamatias@gmail.com>
2025-04-06 19:20:06 +0200

    feat: update file picker setup

--- lib/src/form_builder_file_picker.dart
+++ lib/src/form_builder_file_picker.dart
@@ -14,10 +14,11 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 /// by the [FormBuilderFilePicker].
 ///
 /// [filesSetter] can be used to update the value of [FormBuilderFilePicker].
-typedef FileViewerBuilder = Widget Function(
-  List<PlatformFile>? files,
-  FormFieldSetter<List<PlatformFile>> filesSetter,
-);
+typedef FileViewerBuilder =
+    Widget Function(
+      List<PlatformFile>? files,
+      FormFieldSetter<List<PlatformFile>> filesSetter,
+    );
 
 class TypeSelector {
   final FileType type;
@@ -77,73 +78,89 @@ class FormBuilderFilePicker
   final Widget Function(List<Widget> types)? customTypeViewerBuilder;
 
   /// Creates field for image(s) from user device storage
-  FormBuilderFilePicker(
-      {
-      //From Super
-      super.key,
-      required super.name,
-      super.validator,
-      super.initialValue,
-      super.decoration,
-      super.onChanged,
-      super.valueTransformer,
-      super.enabled,
-      super.onSaved,
-      super.autovalidateMode = AutovalidateMode.disabled,
-      super.onReset,
-      super.focusNode,
-      this.maxFiles,
-      this.withData = kIsWeb,
-      this.withReadStream = false,
-      this.allowMultiple = false,
-      this.previewImages = true,
-      this.typeSelectors = const [
-        TypeSelector(type: FileType.any, selector: Icon(Icons.add_circle))
-      ],
-      this.allowedExtensions,
-      this.onFileLoading,
-      this.allowCompression = true,
-      this.compressionQuality = 30,
-      this.customFileViewerBuilder,
-      this.customTypeViewerBuilder})
-      : super(
-          builder: (FormFieldState<List<PlatformFile>?> field) {
-            final state = field as _FormBuilderFilePickerState;
+  FormBuilderFilePicker({
+    //From Super
+    super.key,
+    required super.name,
+    super.validator,
+    super.initialValue,
+    super.decoration,
+    super.onChanged,
+    super.valueTransformer,
+    super.enabled,
+    super.onSaved,
+    super.autovalidateMode = AutovalidateMode.disabled,
+    super.onReset,
+    super.focusNode,
+    this.maxFiles,
+    this.withData = kIsWeb,
+    this.withReadStream = false,
+    this.allowMultiple = false,
+    this.previewImages = true,
+    this.typeSelectors = const [
+      TypeSelector(type: FileType.any, selector: Icon(Icons.add_circle)),
+    ],
+    this.allowedExtensions,
+    this.onFileLoading,
+    @Deprecated(
+      'allowCompression is deprecated and will be removed in the next major version. '
+      'Use compressionQuality instead.',
+    )
+    this.allowCompression = true,
+    this.compressionQuality = 0,
+    this.customFileViewerBuilder,
+    this.customTypeViewerBuilder,
+  }) : super(
+         builder: (FormFieldState<List<PlatformFile>?> field) {
+           final state = field as _FormBuilderFilePickerState;
 
-            return InputDecorator(
-              decoration: state.decoration.copyWith(
-                  counterText: maxFiles != null
-                      ? '${state._files.length} / $maxFiles'
-                      : null),
-              child: Column(
-                children: <Widget>[
-                  customTypeViewerBuilder != null
-                      ? customTypeViewerBuilder(
-                          state.getTypeSelectorActions(typeSelectors, field))
-                      : Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: state.getTypeSelectorActions(
-                              typeSelectors, field),
-                        ),
-                  const SizedBox(height: 3),
-                  customFileViewerBuilder != null
-                      ? customFileViewerBuilder.call(state._files,
-                          (files) => state._setFiles(files ?? [], field))
-                      : state.defaultFileViewer(state._files,
-                          (files) => state._setFiles(files ?? [], field)),
-                ],
-              ),
-            );
-          },
-        );
+           return InputDecorator(
+             decoration: state.decoration.copyWith(
+               counterText:
+                   maxFiles != null
+                       ? '${state._files.length} / $maxFiles'
+                       : null,
+             ),
+             child: Column(
+               children: <Widget>[
+                 customTypeViewerBuilder != null
+                     ? customTypeViewerBuilder(
+                       state.getTypeSelectorActions(typeSelectors, field),
+                     )
+                     : Row(
+                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                       children: state.getTypeSelectorActions(
+                         typeSelectors,
+                         field,
+                       ),
+                     ),
+                 const SizedBox(height: 3),
+                 customFileViewerBuilder != null
+                     ? customFileViewerBuilder.call(
+                       state._files,
+                       (files) => state._setFiles(files ?? [], field),
+                     )
+                     : state.defaultFileViewer(
+                       state._files,
+                       (files) => state._setFiles(files ?? [], field),
+                     ),
+               ],
+             ),
+           );
+         },
+       );
 
   @override
   FormBuilderFieldDecorationState<FormBuilderFilePicker, List<PlatformFile>>
-      createState() => _FormBuilderFilePickerState();
+  createState() => _FormBuilderFilePickerState();
 }
 
-class _FormBuilderFilePickerState extends FormBuilderFieldDecorationState<
-    FormBuilderFilePicker, List<PlatformFile>> {
+class _FormBuilderFilePickerState
+    extends
+        FormBuilderFieldDecorationState<
+          FormBuilderFilePicker,
+          List<PlatformFile>
+        > {
   /// Image File Extensions.
   ///
   /// Note that images may be previewed.
@@ -175,14 +192,15 @@ class _FormBuilderFilePickerState extends FormBuilderFieldDecorationState<
   }
 
   Future<void> pickFiles(
-      FormFieldState<List<PlatformFile>?> field, FileType fileType) async {
+    FormFieldState<List<PlatformFile>?> field,
+    FileType fileType,
+  ) async {
     FilePickerResult? resultList;
 
     try {
       resultList = await FilePicker.platform.pickFiles(
         type: fileType,
         allowedExtensions: widget.allowedExtensions,
-        allowCompression: widget.allowCompression,
         compressionQuality: widget.compressionQuality,
         onFileLoading: widget.onFileLoading,
         allowMultiple: widget.allowMultiple,
@@ -205,7 +223,9 @@ class _FormBuilderFilePickerState extends FormBuilderFieldDecorationState<
   }
 
   void _setFiles(
-      List<PlatformFile> files, FormFieldState<List<PlatformFile>?> field) {
+    List<PlatformFile> files,
+    FormFieldState<List<PlatformFile>?> field,
+  ) {
     setState(() => _files = files);
     field.didChange(_files);
   }
@@ -216,7 +236,9 @@ class _FormBuilderFilePickerState extends FormBuilderFieldDecorationState<
   }
 
   Widget defaultFileViewer(
-      List<PlatformFile> files, FormFieldSetter<List<PlatformFile>> setter) {
+    List<PlatformFile> files,
+    FormFieldSetter<List<PlatformFile>> setter,
+  ) {
     final theme = Theme.of(context);
 
     return LayoutBuilder(
@@ -230,27 +252,31 @@ class _FormBuilderFilePickerState extends FormBuilderFieldDecorationState<
           runAlignment: WrapAlignment.start,
           runSpacing: 10,
           spacing: 10,
-          children: List.generate(
-            files.length,
-            (index) {
-              return Container(
-                height: itemSize,
-                width: itemSize,
-                margin: const EdgeInsets.only(right: 2),
-                child: Stack(
-                  alignment: Alignment.bottomCenter,
-                  children: <Widget>[
-                    Container(
-                      alignment: Alignment.center,
-                      child: (imageFileExts.contains(
-                                  files[index].extension!.toLowerCase()) &&
-                              widget.previewImages)
-                          ? widget.withData
-                              ? Image.memory(files[index].bytes!,
-                                  fit: BoxFit.cover)
-                              : Image.file(File(files[index].path!),
-                                  fit: BoxFit.cover)
-                          : Container(
+          children: List.generate(files.length, (index) {
+            return Container(
+              height: itemSize,
+              width: itemSize,
+              margin: const EdgeInsets.only(right: 2),
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.center,
+                    child:
+                        (imageFileExts.contains(
+                                  files[index].extension!.toLowerCase(),
+                                ) &&
+                                widget.previewImages)
+                            ? widget.withData
+                                ? Image.memory(
+                                  files[index].bytes!,
+                                  fit: BoxFit.cover,
+                                )
+                                : Image.file(
+                                  File(files[index].path!),
+                                  fit: BoxFit.cover,
+                                )
+                            : Container(
                               alignment: Alignment.center,
                               color: theme.primaryColor,
                               child: Icon(
@@ -259,63 +285,65 @@ class _FormBuilderFilePickerState extends FormBuilderFieldDecorationState<
                                 size: 56,
                               ),
                             ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 2),
+                    width: double.infinity,
+                    color: Colors.white.withValues(alpha: .8),
+                    child: Text(
+                      files[index].name,
+                      style: theme.textTheme.bodySmall,
+                      maxLines: 2,
+                      overflow: TextOverflow.clip,
                     ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 2),
-                      width: double.infinity,
-                      color: Colors.white.withValues(alpha: .8),
-                      child: Text(
-                        files[index].name,
-                        style: theme.textTheme.bodySmall,
-                        maxLines: 2,
-                        overflow: TextOverflow.clip,
-                      ),
-                    ),
-                    if (enabled)
-                      Positioned(
-                        top: 0,
-                        right: 0,
-                        child: InkWell(
-                          onTap: () {
-                            files.removeAt(index);
-                            setter.call([...files]);
-                          },
-                          child: Container(
-                            margin: const EdgeInsets.all(3),
-                            decoration: BoxDecoration(
-                              color: Colors.grey.withValues(alpha: .7),
-                              shape: BoxShape.circle,
-                            ),
-                            alignment: Alignment.center,
-                            height: 22,
-                            width: 22,
-                            child: const Icon(
-                              Icons.close,
-                              size: 18,
-                              color: Colors.white,
-                            ),
+                  ),
+                  if (enabled)
+                    Positioned(
+                      top: 0,
+                      right: 0,
+                      child: InkWell(
+                        onTap: () {
+                          files.removeAt(index);
+                          setter.call([...files]);
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.all(3),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.withValues(alpha: .7),
+                            shape: BoxShape.circle,
+                          ),
+                          alignment: Alignment.center,
+                          height: 22,
+                          width: 22,
+                          child: const Icon(
+                            Icons.close,
+                            size: 18,
+                            color: Colors.white,
                           ),
                         ),
                       ),
-                  ],
-                ),
-              );
-            },
-          ),
+                    ),
+                ],
+              ),
+            );
+          }),
         );
       },
     );
   }
 
-  List<Widget> getTypeSelectorActions(List<TypeSelector> typeSelectors,
-      FormFieldState<List<PlatformFile>?> field) {
+  List<Widget> getTypeSelectorActions(
+    List<TypeSelector> typeSelectors,
+    FormFieldState<List<PlatformFile>?> field,
+  ) {
     return <Widget>[
       ...typeSelectors.map(
         (typeSelector) => InkWell(
-          onTap: enabled &&
-                  (null == _remainingItemCount || _remainingItemCount! > 0)
-              ? () => pickFiles(field, typeSelector.type)
-              : null,
+          onTap:
+              enabled &&
+                      (null == _remainingItemCount || _remainingItemCount! > 0)
+                  ? () => pickFiles(field, typeSelector.type)
+                  : null,
           child: typeSelector.selector,
         ),
       ),
